f2d15cd833c6da0f1318092221e1aa5909e017ca
Marcin <kalik95@gmail.com>
2020-04-29 23:08:45 +0200

    Sync (others video) request and handler, manual websocket connect and disconnect, bug fixes

--- plugin/sync-plugin/video-ctl.js
+++ plugin/sync-plugin/video-ctl.js
@@ -2,7 +2,7 @@ var videoElement = undefined;
 var lastLoadedPage = undefined;
 var lag = -1;
 
-function loadVideoElement(isFromSocket = false)
+function loadVideoElement()
 {
 	console.log(document);
 	videoElement = document.getElementsByTagName("video")[0];
@@ -32,15 +32,20 @@ function playVideo(isFromSocket = false)
 		videoElement.play();
 		if (!isFromSocket)
 		{
-			console.warn("Sending!");
-			console.log(hestiaWebsocketConnection);
-			console.log("is" + isConnectionGenerated);
-			hestiaWebsocketConnection.send("PLAY");
+			if (isConnectionGenerated)
+			{
+				console.info("Sending play command!");
+				hestiaWebsocketConnection.send("PLAY");
+			}
+			else
+			{
+				console.warn("Play command not sent - connection was not generated!");
+			}
 		}
 	}
 	else
 	{
-		console.warn("Video is undefined or null");
+		console.warn("Cannot play video. Video is undefined or null");
 	}
 }
 
@@ -51,29 +56,57 @@ function pauseVideo(isFromSocket = false)
 		videoElement.pause();
 		if (!isFromSocket)
 		{
-			console.warn("Sending!");
-			console.log(hestiaWebsocketConnection);
-			console.log("is" + isConnectionGenerated);
-			hestiaWebsocketConnection.send("PAUSE");
+			if (isConnectionGenerated)
+			{
+				console.info("Sending pause command!");
+				hestiaWebsocketConnection.send("PAUSE");
+			}
+			else
+			{
+				console.warn("Pause command not sent - connection was not generated!");
+			}
 		}
 	}
 	else
 	{
-		console.warn("Video is undefined or null");
+		console.warn("Cannot pause. Video is undefined or null");
 	}
 }
 
-function syncVideo(isFromSocket = false)
+function syncRequest()
 {
 	if (videoElement)
 	{
-		videoElement.currentTime = 20
-		generateConnection();
-		console.log(hestiaWebsocketConnection);
+		if (isConnectionGenerated)
+		{
+			console.info("Sending sync command!");
+			hestiaWebsocketConnection.send("SYNC-T" + videoElement.currentTime);
+		}
+		else
+		{
+			console.warn("Sync command not sent - connection was not generated!");
+		}
 	}
 	else
 	{
-		console.warn("Video is undefined or null");
+		console.warn("Sannot send sync request. Video is undefined or null");
+	}
+}
+
+function syncVideo(command)
+{
+	if (videoElement)
+	{
+		const time = command.split('T');
+		if (time[1])
+		{
+			videoElement.currentTime = time[1];
+			console.log("Yours video was synced to time: " + time[1]);
+		}
+	}
+	else
+	{
+		console.warn("Cannot sync. Video is undefined or null");
 	}
 }
 
@@ -88,8 +121,9 @@ function showPageUrl(isFromSocket = false)
 
 function lagMeasure(isFromSocket = false)
 {
+/*
 	lag--;
 	console.log("Lag: " + lag);
 	//destroyCtrlPanel();
-	closeConnection();
+*/
 }
