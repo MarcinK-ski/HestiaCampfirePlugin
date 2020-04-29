0750fcd23b273ca8d792fa117176c87651c3fe41
Marcin <kalik95@gmail.com>
2020-04-29 21:58:56 +0200

    Squashed commit of the following:
    commit dc45d1db422e97e48e9040d5cc271e5981a23c40
    Author: Marcin <kalik95@gmail.com>
    Date:   Wed Apr 29 21:57:06 2020 +0200
    
        Server PoC
    
    commit 8c79b991a889e52494ed6e4efb6c822775701443
    Author: Marcin <kalik95@gmail.com>
    Date:   Wed Apr 29 21:55:28 2020 +0200
    
        gitignore update for server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+var roomsUsersConnectionDictionary = [];
+
+wss.on('connection', function connection(ws, req) {
+    console.log('connected');
+
+    ws.id = req.headers['sec-websocket-key'];
+    console.log(ws.id);
+
+    ws.room = "01";
+    if (roomsUsersConnectionDictionary[ws.room]) {
+        roomsUsersConnectionDictionary[ws.room].push(ws);
+    } else {
+        roomsUsersConnectionDictionary[ws.room] = [ws];
+    }
+    console.log(ws.room);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        console.log(ws.id);
+
+        roomsUsersConnectionDictionary[ws.room].forEach(sock => {
+            if (sock.id !== ws.id) {
+                sock.send(message);
+            }
+        });
+    });
+
+    ws.send('CONNECTED TO ROOM:' + ws.room);
+});
