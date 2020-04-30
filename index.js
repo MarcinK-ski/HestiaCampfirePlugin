2006a15605900b0f2d922f7f8993796b3778ef0c
Marcin <kalik95@gmail.com>
2020-04-30 19:06:46 +0200

    Server port fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const WebSocket = require('ws');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const INDEX_HTML = "overview.html";
 
 var roomsUsersConnectionDictionary = [];
