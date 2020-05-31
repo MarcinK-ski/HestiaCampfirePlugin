0a094de55f5d9c106acb76de4c092386c626f196
Marcin K <kalik95@gmail.com>
2020-05-31 13:10:27 +0200

    Comments in user-types.js + "Disconnected" type + check is passed userType, has permission to do sth

--- firefox-plugin/user-types.js
+++ firefox-plugin/user-types.js
@@ -1,7 +1,14 @@
 const userTypes =
 {
-    "GUEST-D": "GUEST-D",
-    "GUEST-V": "GUEST-V",
-    "HOST-T": "HOST-T",
-    HOST: "HOST"
+    "DISCONNECTED": "DISCONNECTED", // User is not connected to server
+    "GUEST-D": "GUEST-D", // Guest-D is not verified Guest type.
+    "GUEST-V": "GUEST-V", // Guest-V is verified on server side Guest type.
+    "HOST-T": "HOST-T", // Host-T is Temp Host type.
+    "HOST": "HOST" // Host is the oldest user on server - by default - sync and grand HOST-T is depends on this user
 };
+
+isUserTypeWithPermission = (currentUserType) =>
+{
+    return currentUserType === userTypes["HOST"]
+        || currentUserType === userTypes["HOST-T"];
+}
